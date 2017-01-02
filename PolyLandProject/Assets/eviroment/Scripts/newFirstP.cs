09de42bc55e77b78049db791b39904d3e9f2e1b1
snowydawolf <snowydawolf@gmail.com>
2017-01-02 17:35:03 +0000

    eeeeeeeeeeeee working on script

---
+++ PolyLandProject/Assets/eviroment/Scripts/newFirstP.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class newFirstP : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float front = Input.GetAxis("Vertical");
+        Vector3 speed = new Vector3(0, 0, front);
+        CharacterController c = GetComponent<CharacterController>();
+        c.SimpleMove(speed);
+	}
+}
