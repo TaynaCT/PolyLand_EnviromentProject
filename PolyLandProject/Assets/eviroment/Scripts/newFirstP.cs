3297d07c8cb1821f538b606a6509a243a87e5cce
snowydawolf <snowydawolf@gmail.com>
2017-01-02 18:43:37 +0000

    Things move

--- PolyLandProject/Assets/eviroment/Scripts/newFirstP.cs
+++ PolyLandProject/Assets/eviroment/Scripts/newFirstP.cs
@@ -3,17 +3,26 @@ using System.Collections;
 
 public class newFirstP : MonoBehaviour {
 
+    Transform playerT;
+
 	// Use this for initialization
 	void Start () {
-	
+        playerT = transform;
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-
-        float front = Input.GetAxis("Vertical");
-        Vector3 speed = new Vector3(0, 0, front);
-        CharacterController c = GetComponent<CharacterController>();
-        c.SimpleMove(speed);
+	void Update () {
+        MovePlayer();
 	}
+
+    void MovePlayer()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+        float moveJ = Input.GetAxis("Jump");
+
+        Vector3 movement = new Vector3(moveH, moveJ, moveV);
+
+        playerT.position += movement * Time.deltaTime;
+    }
 }
