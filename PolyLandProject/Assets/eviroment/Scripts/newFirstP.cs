51b90f7c0f0b8ae2bc60fc3eabdffb558f415433
snowydawolf <snowydawolf@gmail.com>
2017-01-06 10:33:49 +0000

    Limitações no Y da camera.

--- PolyLandProject/Assets/eviroment/Scripts/newFirstP.cs
+++ PolyLandProject/Assets/eviroment/Scripts/newFirstP.cs
@@ -5,6 +5,9 @@ public class newFirstP : MonoBehaviour {
 
     Transform playerT;
     public float mSpeed;
+    public float rangeY = 75f;
+
+    public float rotateV = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -19,10 +22,11 @@ public class newFirstP : MonoBehaviour {
     void MovePlayer()
     {
         float rotateH = Input.GetAxis("Mouse X");
-        float rotateV = Input.GetAxis("Mouse Y");
-
         transform.Rotate(0, rotateH, 0);
-        Camera.main.transform.Rotate(-rotateV, 0, 0);
+
+        rotateV -= Input.GetAxis("Mouse Y");
+        rotateV = Mathf.Clamp(rotateV, -rangeY, rangeY);
+        Camera.main.transform.localRotation = Quaternion.Euler(rotateV,0,0);
 
         
         //Player M
