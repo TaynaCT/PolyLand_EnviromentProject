f1f9416e9929fd3c9d06f3bd2cb28c2321a37a8a
snowydawolf <snowydawolf@gmail.com>
2017-01-08 23:22:01 +0000

    I don't even know what this shit is about anymore :V

--- PolyLandProject/Assets/eviroment/Scripts/newEnemies.cs
+++ PolyLandProject/Assets/eviroment/Scripts/newEnemies.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class newEnemies : MonoBehaviour {
 
+
+    GameObject Player;
+    Transform EnemyT;
+
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        Player = GameObject.FindGameObjectWithTag("PlayerMe");
+        EnemyT = Player.transform;
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+        Move();
 	}
+
+    void Move()
+    {
+        Vector3 move = EnemyT.position - transform.position;
+        move.Normalize();
+        transform.position += move * Time.deltaTime;
+    }
+
+
 }
